087e1691a7faf221e366f186645d1b6b6fe929d5
pietrop <pietro.passarelli@gmail.com>
2015-03-29 12:56:41 +0100

    changed root

--- InteractiveElections/config/routes.rb
+++ InteractiveElections/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'tv/index'
+  # get 'tv/index'
 
 
   # get 'welcome/index'
- root 'welcome#index'
+ root 'tv#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
